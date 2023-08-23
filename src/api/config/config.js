eab60d5b23502e4b39ba97396a30f0b9a1cb707b
Mayank Gautam <mayankgautam2105@gmail.com>
2023-08-24 03:51:18 +0530

    updates

--- src/api/config/config.js
+++ src/api/config/config.js
@@ -5,7 +5,7 @@ dotenv.config();
 export default {
     apiVersionUrl:"/api/v1",
     appPort:3001,
-    numberOfProxies:1,
+    numberOfProxies:process.env.NO_OF_PROXY,
     fileSizeLimit:'100mb',
     whitelistUrl:[/\localhost\$/,/\distinctcloud\.io$/,/\signettags\.com$/,'http://localhost:3000','https://sample-react-eta.vercel.app'],
     apiLevelJWTExpiry:'90d',
