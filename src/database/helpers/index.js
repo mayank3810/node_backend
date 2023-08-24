666bbf43528e38fd2023e4fa2bebed9f100d0fcc
Mayank Gautam <mayankgautam2105@gmail.com>
2023-08-24 13:30:49 +0530

    update on db helper

--- src/database/helpers/index.js
+++ src/database/helpers/index.js
@@ -1,9 +1,11 @@
-const findOne = async (model,payload) => {
+import catchAsync from "../../api/helpers/catchAsync.js";
+
+const findOne = catchAsync(async (model, payload) => {
     return model.findOne(payload);
-}
+});
 
-const create = async (model,payload) => {
+const create = catchAsync(async (model, payload) => {
     return model.create(payload);
-}
+});
 
-export {findOne,create};
+export { findOne, create };
