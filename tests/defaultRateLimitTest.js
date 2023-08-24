ea55ab973292a0366b7c80523b8f540a62afe3bc
Mayank Gautam <mayankgautam2105@gmail.com>
2023-08-24 11:37:31 +0530

    rate limiter

--- tests/defaultRateLimitTest.js
+++ tests/defaultRateLimitTest.js
@@ -5,11 +5,11 @@ export let options ={
     insecureSkipTLSVerify: true,
     noConnectionReuse: false,
     vus: 1,
-    duration: '60s'
+    duration: '30s'
 
 };
 
 export default () => {
-    http.get('http://localhost:3001/api/v1/users/getAllUsers')
-    sleep(0.5)
+    http.get('https://node-backend-2q4t.onrender.com/api/v1')
+    // sleep(0.5)
 };
